c0a187a28ae137a249ec35f58a84d543a9e4e9a5
luca <luca_hendrik_martin.rommler@student.reutlingen-university.de>
2024-06-16 17:49:30 +0200

    Fix

--- src/main/SQLDriver.go
+++ src/main/SQLDriver.go
@@ -12,6 +12,12 @@ var db *sql.DB
 func initDBDriver() {
 	fmt.Println("Initializing DB Driver...")
 
+	fmt.Println("Creating connection with parameters: ")
+	fmt.Println("DB_USER: ", DB_USER)
+	fmt.Println("DB_PASS: ", DB_PASS)
+	fmt.Println("DB_NAME: ", DB_NAME)
+	fmt.Println("DB_HOST: ", DB_HOST)
+	fmt.Println("DB_PORT: ", DB_PORT)
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 
 	var err error
